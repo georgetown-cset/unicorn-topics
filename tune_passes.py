8121b91da79d40db34eeb39700f5f7a9b50d7fba
Rebecca <rggelles@gmail.com>
2020-09-22 15:23:27 -0400

    Tuning beta some more, adding more chunksize granularity

--- tune_passes.py
+++ tune_passes.py
@@ -154,37 +154,38 @@ class TopicModel:
         self.id2word = corpora.Dictionary(self.documents)
         self.id2word.filter_extremes(no_below=20, no_above=0.5)
         corpus = [self.id2word.doc2bow(text) for text in self.documents]
-        passes = [50, 60]
-        chunksize = [25, 50, 100, 150, 200]
+        beta = [0.7000000000000001, 0.8, 0.9]
+        chunksize = [50, 75, 100]
         corpus_sets = [gensim.utils.ClippedCorpus(corpus, int(len(corpus)*0.75)), corpus]
         corpus_titles = ["75% corpus", "100% corpus"]
-        model_results = {"Validation_set": [], "Topics": [], "Passes": [], "Chunksize": [], "Coherence": []}
+        model_results = {"Validation_set": [], "Topics": [], "Beta": [], "Chunksize": [], "Coherence": []}
         print("Fitting models")
         for i, corpus_set in enumerate(corpus_sets):
             for num_topics in self.topics_to_test:
-                for p in passes:
+                for b in beta:
                     for c in chunksize:
                         lda_model = gensim.models.LdaMulticore(corpus=corpus_set, id2word=self.id2word,
                                                                alpha="symmetric", random_state=100, chunksize=c,
-                                                               passes=p, num_topics=num_topics, per_word_topics=True,
-                                                               minimum_probability=0, eta=0.7000000000000001)
+                                                               passes=60, num_topics=num_topics, per_word_topics=True,
+                                                               minimum_probability=0, eta=b)
                         if i == 1: # we only want to save the model if it's a model on the whole corpus
                             if not os.path.exists(f"data/intermediate/passes_testing"):
                                 os.mkdir(f"data/intermediate/passes_testing")
                             with open(f"data/intermediate/passes_testing/lda_{num_topics}_"
-                                      f"topics{p}_passes_{c}_chunksize.pkl", "wb") as file_out:
+                                      f"topics{b}_beta_{c}_chunksize.pkl", "wb") as file_out:
                                 pickle.dump(lda_model, file_out)
                         coherence_model_lda = CoherenceModel(model=lda_model, texts=self.documents,
                                              dictionary=self.id2word, coherence='c_v')
                         coherence = coherence_model_lda.get_coherence()
-                        print(f"Topic {num_topics}, passes {p} chunksize {c} corpus {corpus_titles[i]} "
+                        print(f"Topic {num_topics}, beta {b} chunksize {c} corpus {corpus_titles[i]} "
                               f"coherence: {coherence}")
                         model_results['Validation_set'].append(corpus_titles[i])
                         model_results['Topics'].append(num_topics)
-                        model_results['Passes'].append(p)
+                        model_results['Beta'].append(b)
                         model_results['Chunksize'].append(c)
                         model_results['Coherence'].append(coherence)
-        # coherence_model_lda = CoherenceModel(model=lda_model, texts=self.documents, dictionary=self.id2word,
+        # coherence_model_lda = C
+        # CoherenceModel(model=lda_model, texts=self.documents, dictionary=self.id2word,
         #                                  coherence='u_mass')
         # coherence_u_mass.append(coherence_model_lda.get_coherence())
         pd.DataFrame(model_results).to_csv("passes_tuning_results.csv", index=False)
